d27f7e2194148264d23996330a467bf73ed318fd
DenDmitriev <dv.denstr@gmail.com>
2023-11-13 13:15:06 +0300

    Initial Commit

---
+++ SkifTestDmitriev/SkifTestDmitrievApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkifTestDmitrievApp.swift
+//  SkifTestDmitriev
+//
+//  Created by Denis Dmitriev on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SkifTestDmitrievApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

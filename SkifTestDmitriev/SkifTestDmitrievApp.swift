de04afc480bba1e381b2837a2158736095910411
DenDmitriev <dv.denstr@gmail.com>
2023-11-13 13:59:09 +0300

    first init

--- SkifTestDmitriev/SkifTestDmitrievApp.swift
+++ SkifTestDmitriev/SkifTestDmitrievApp.swift
@@ -6,6 +6,14 @@
 //
 
 import SwiftUI
+import GoogleMaps
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        GMSServices.provideAPIKey("")
+        return true
+    }
+}
 
 @main
 struct SkifTestDmitrievApp: App {
